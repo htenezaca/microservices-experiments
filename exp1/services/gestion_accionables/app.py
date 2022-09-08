5f2dc37bab58fe9619c896add61efa16367bb89e
miso-alejosaur <a.santamaria11@uniandes.edu.co>
2022-09-08 04:27:15 -0500

    network errors

--- exp1/services/gestion_accionables/app.py
+++ exp1/services/gestion_accionables/app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, redirect
+import random
 
 app = Flask(__name__)
+threshold = 0.5
 
 @app.get('/')
 def hello_world():
-    return {'message': 'Hello, World! from gestion_accionables'}
+    random_number = random.random()
+    print(random_number)
+    if(random_number < threshold):
+        if(random_number < threshold/2):
+            #ECONNREFUSED
+            return redirect('http://127.0.0.1:8000')
+        else:
+            #Timeout
+            return redirect('http://10.0.0.0')
+    else:
+        #Ok
+        return {'message': 'Hello, World! from gestion_accionables'}
