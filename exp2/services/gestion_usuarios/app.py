1dcdec62e42bd5738196934001d21fdc87c72fc8
Hector Tenezaca <h.tenezaca@uniandes.edu.co>
2022-09-27 15:51:29 -0500

    feat: gestion_usuarios funcs

--- exp2/services/gestion_usuarios/app.py
+++ exp2/services/gestion_usuarios/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_restful import Api, Resource
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required
 from flask_jwt_extended import JWTManager
 import requests
 import logging
@@ -22,10 +22,10 @@ users = {
 @app.get("/")
 def index():
     app.logger.info("Index page accessed")
-    return {"message": "Hello World, I authenticate the users"}
+    return {"message": "Hello World, I authenticate the users"}, 200
 
 
-class AuthResource(Resource):
+class Auth(Resource):
     def post(self):
         app.logger.info("Authenticating user")
         username = request.json.get("username", None)
@@ -33,7 +33,69 @@ class AuthResource(Resource):
         if username not in users or password != users[username]["password"]:
             return {"error": "Bad username or password"}, 401
         access_token = create_access_token(identity=username)
-        return {"access_token": access_token}, 200
+        response = self.notificar(access_token)
+        # No send access_token, just for example
+        return {"message": "succesfull"}, 200
+        # return {"access_token": access_token}, 200
+
+    def notificar(self, token):
+        # TODO: Send the request for double authentication Chamge this!
+        # Get the response from the notificador service
+        # return requests.post("http://notificador:5000/comandos/notificador", headers={"Authorization": f"Bearer {token}"})
+        return requests.get(f"http://api_gateway/comandos/notificador", headers={"Authorization": f"Bearer {token}"})
 
 
-api.add_resource(AuthResource, '/auth')
+class TwoFactor(Resource):
+
+    two_factor = '123456'
+
+    @jwt_required()
+    def get(self):
+        return {"token": self.two_factor}, 200
+
+    def get_value(self):
+        return self.two_factor
+
+
+class Auth2(Resource):
+
+    two_factor_generated = TwoFactor()
+
+    @jwt_required()
+    def get(self):
+        two_factor_input = request.args.get('token')
+        if two_factor_input != self.two_factor_generated.get_value():
+            return {"error": "Two_token doesn't match"}, 401
+        self.notifyToClient(self.get_token_header())
+        return {"message": self.get_token_header()}, 200
+
+    def get_token_header(self):
+        return request.headers.get('Authorization')
+
+    def notifyToClient(self, token):
+        # TODO: Send the request for double authentication Chamge this!
+        return requests.get(f"http://api_gateway/comandos/client_web", headers={"Authorization": f"{token}"})
+
+
+class NotifyToClient(Resource):
+
+    @jwt_required()
+    def post(self):
+        app.logger.info("Sending message to user")
+        token = request.headers.get("Authorization", None)
+        if token is None:
+            return {"error": "Missing token"}, 401
+        if self.notify(token).ok:
+            return {"message": "Message sent"}, 200
+        return {"error": "Error sending message"}, 500
+
+    def notify(self, token):
+        # TODO: Send the notification to the user Change this!
+        # return requests.post("http://notificador:5000/comandos/cliente_web")
+        return requests.get(f"http://api_gateway/comandos/cliente_web")
+
+
+api.add_resource(Auth, '/auth')
+api.add_resource(NotifyToClient, '/notificar')
+api.add_resource(Auth2, '/auth2')
+api.add_resource(TwoFactor, '/two_factor')
