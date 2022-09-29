25a675be726292fd294d5b1591d17323fe04c96f
miso-alejosaur <a.santamaria11@uniandes.edu.co>
2022-09-29 06:02:56 -0500

    notifier

--- exp2/services/notificador/app.py
+++ exp2/services/notificador/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 import logging
+import requests
 
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
@@ -8,3 +9,20 @@ app.logger.setLevel(logging.INFO)
 def index():
     app.logger.info("Index page accessed")
     return {"message": "Hello World, I send notifications to users"}
+
+@app.post("/2fa")
+def send_token():
+    token = request.json.get("token_2fa", None)
+    if(send_notification(token)):
+        return {"message":"user notified"}, 200
+    else:
+        return {"message":"error notifying"}, 500
+
+
+def send_notification(token_2fa):
+    res = requests.get(
+        f"http://cliente_web:5000/client_2fa", json={"token_2fa": token_2fa})
+    if res.status_code == 200:
+        return True
+    else:
+        return False
