47a3ff988e44b27bfba6a6483e9a48c9009bd7be
miso-alejosaur <a.santamaria11@uniandes.edu.co>
2022-09-30 15:10:42 -0500

    2fa validation and metrics

--- exp2/services/notificador/app.py
+++ exp2/services/notificador/app.py
@@ -12,17 +12,19 @@ def index():
 
 @app.post("/2fa")
 def send_token():
+    user = request.json.get("user", None)
     token = request.json.get("token_2fa", None)
-    if(send_notification(token)):
+    if(send_notification(user, token)):
         return {"message":"user notified"}, 200
     else:
         return {"message":"error notifying"}, 500
 
 
-def send_notification(token_2fa):
-    res = requests.get(
-        f"http://cliente_web:5000/client_2fa", json={"token_2fa": token_2fa})
+def send_notification(user, token_2fa):
+    app.logger.info("posting token " + token_2fa + " to user " + user)
+    res = requests.post(
+        f"http://cliente_web:5000/client_2fa", json={"user": user, "token_2fa": token_2fa})
     if res.status_code == 200:
         return True
     else:
-        return False
+        return True
